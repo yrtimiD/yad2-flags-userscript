eee97025583c9c0396fc23df4d7aa5edd3e39ff0
Dmitry Gurovich <yrtimid@gmail.com>
2022-04-29 21:50:41 +0300

    0.6 Flags on single item page

--- yad2-flags.user.js
+++ yad2-flags.user.js
@@ -1,20 +1,22 @@
 // ==UserScript==
 // @name         yad2-flags
 // @namespace    http://tampermonkey.net/
-// @version      0.5
+// @version      0.6
 // @description  Adds dedicated flag buttons for easier marking of real estate search results. Currently "pin", "done" and "hide" flags are supported. Adding new flags is super easy.
 // @author       Dmitry Gurovich
 // @license      UNLICENSE
 // @website      https://github.com/yrtimiD/yad2-flags-userscript
 // @supportURL   https://github.com/yrtimiD/yad2-flags-userscript/issues
 // @match        https://www.yad2.co.il/realestate/*
+// @match        https://www.yad2.co.il/item/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=yad2.co.il
 // @grant        none
 // @run-at       document-end
 // ==/UserScript==
 
 /**
- * Changelog
+ * Changelog:
+ * 0.6 Flags on single item page
  * 0.5 Better icons, moved icons container to the search item start
  */
 (function () {
@@ -32,9 +34,9 @@
 		hidden: { icon: 'icon-hidden', tooltip: 'Hide item', class: `${PREFIX}-hidden` },
 	};
 
-	function setupSearchPageDependencies() {
+	function setupCommonDependencies() {
 		const frag = document.createRange().createContextualFragment(`
-		<style>
+		<style type="text/css">
 			input.${PREFIX}-icon-save {
 				background-image: url('data:image/svg+xml,<svg xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid meet" viewBox="0 0 24 24"><path fill="currentColor" d="M18 2H6c-1.103 0-2 .897-2 2v18l8-4.572L20 22V4c0-1.103-.897-2-2-2zm0 16.553l-6-3.428l-6 3.428V4h12v14.553z"/></svg>');
 			}
@@ -59,16 +61,6 @@
 				background-image: url('data:image/svg+xml,<svg xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid meet" viewBox="0 0 24 24"><path fill="currentColor" d="M12 19c.946 0 1.81-.103 2.598-.281l-1.757-1.757c-.273.021-.55.038-.841.038c-5.351 0-7.424-3.846-7.926-5a8.642 8.642 0 0 1 1.508-2.297L4.184 8.305c-1.538 1.667-2.121 3.346-2.132 3.379a.994.994 0 0 0 0 .633C2.073 12.383 4.367 19 12 19zm0-14c-1.837 0-3.346.396-4.604.981L3.707 2.293L2.293 3.707l18 18l1.414-1.414l-3.319-3.319c2.614-1.951 3.547-4.615 3.561-4.657a.994.994 0 0 0 0-.633C21.927 11.617 19.633 5 12 5zm4.972 10.558l-2.28-2.28c.19-.39.308-.819.308-1.278c0-1.641-1.359-3-3-3c-.459 0-.888.118-1.277.309L8.915 7.501A9.26 9.26 0 0 1 12 7c5.351 0 7.424 3.846 7.926 5c-.302.692-1.166 2.342-2.954 3.558z"/></svg>');
 			}
 
-			.feeditem {
-				display: flex; // fixes layout for our buttons container
-			}
-
-			.${PREFIX}-buttons {
-				display: flex;
-				flex-direction: column;
-				justify-content: space-evenly;
-			}
-
 			.${PREFIX}-button {
 				width: 16px;
 				height: 16px;
@@ -84,6 +76,47 @@
 			}
 			.${FLAGS.hidden.class}{
 				opacity:30%;
+			}
+		</style>`);
+		document.querySelector('head').append(frag);
+	}
+
+	function setupSearchPageDependencies() {
+		const frag = document.createRange().createContextualFragment(`
+		<style type="text/css">
+			.feeditem {
+				display: flex; // fixes layout for our buttons container
+			}
+
+			.${PREFIX}-buttons {
+				display: flex;
+				flex-direction: column;
+				justify-content: space-evenly;
+			}
+
+			.${PREFIX}-button {
+				width: 16px;
+				height: 16px;
+			}
+		</style>
+		`);
+		document.querySelector('head').append(frag);
+	}
+
+	function setupItemPageDependencies() {
+		const frag = document.createRange().createContextualFragment(`
+		<style type="text/css">
+			--${PREFIX}-icon-size: 24px;
+
+			.${PREFIX}-buttons {
+				display: flex;
+				flex-direction: row;
+				justify-content: space-evenly;
+			}
+
+			.${PREFIX}-button {
+				width: 24px;
+				height: 24px;
 			}
 		</style>
 		`);
@@ -121,7 +154,7 @@
 	 */
 	function addButton(flag, id, container, ele) {
 		let state = getFlag(flag, id, false);
-		let frag = document.createRange().createContextualFragment(`<input type="checkbox"${state ?" checked":""} title="${FLAGS[flag].tooltip}" class="${PREFIX}-button ${PREFIX}-icon-${flag}" />`);
+		let frag = document.createRange().createContextualFragment(`<input type="checkbox"${state ? " checked" : ""} title="${FLAGS[flag].tooltip}" class="${PREFIX}-button ${PREFIX}-icon-${flag}" />`);
 		frag.children[0].addEventListener('change', (e) => { toggleFlag(flag, id, ele, e.target.checked); e.stopPropagation(); });
 		container.append(frag);
 	}
@@ -153,8 +186,6 @@
 	}
 
 	function initSearchPageMode() {
-		setupSearchPageDependencies();
-
 		const observer = new MutationObserver(() => update());
 		observer.observe(document.body, { childList: true, subtree: true });
 
@@ -164,7 +195,8 @@
 	function initItemPageMode() {
 		let id = window.location.pathname.match(/^\/item\/([A-Za-z0-9]+)/)?.[1];
 		if (id) {
-			let container = document.querySelector('.like_icon_wrapper');
+			document.querySelector('.like_icon_wrapper').insertAdjacentHTML('afterbegin', `<div class="${PREFIX}-buttons"></div>`);
+			let container = document.querySelector(`.${PREFIX}-buttons`);
 			let itemElement = document.querySelector('.top_components');
 			Object.keys(FLAGS).forEach(flag => {
 				addButton(flag, id, container, itemElement);
@@ -173,9 +205,12 @@
 		}
 	}
 
+	setupCommonDependencies();
 	if (/^\/item\//.test(window.location.pathname)) {
+		setupItemPageDependencies();
 		initItemPageMode();
 	} else {
+		setupSearchPageDependencies();
 		initSearchPageMode();
 	}
 })();
